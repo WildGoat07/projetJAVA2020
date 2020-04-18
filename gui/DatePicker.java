c5b594f1b7a4179ca094f040824352bc118107fd
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-04-18 17:03:37 +0200

    date picker can return null

--- gui/DatePicker.java
+++ gui/DatePicker.java
@@ -1,30 +1,43 @@
 package gui;
 
 import java.util.*;
+import java.io.*;
 
 import java.awt.event.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import javax.swing.event.*;
 
 import java.awt.*;
-import java.text.DateFormatSymbols;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
+import java.time.format.*;
 
+/**
+ * A date picker is a dialog that let the user select a date
+ */
 public class DatePicker extends JDialog {
     private Locale language;
     private JSpinner year;
     private JLabel month;
     private JPanel days;
+    private LocalDate usedDate;
     private LocalDate result;
-    public DatePicker(Locale lang) {
+    /**
+     * Constructor
+     * @param date starting date to use
+     * @param lang language to use
+     */
+    public DatePicker(LocalDate date, Locale lang) {
         language = lang;
-        result = LocalDate.now();
-        setSize(350, 200);
+        usedDate = date;
+        setSize(360, 210);
+        try {
+            setIconImage(ImageIO.read(new File("images/cal.png")));
+        }
+        catch(Exception e){}
         setLayout(new BorderLayout());
         {
             JPanel bar = new JPanel();
@@ -34,12 +47,12 @@ public class DatePicker extends JDialog {
             previous.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (result.getMonth().getValue() == 1) {
-                        result = LocalDate.of(result.getYear()-1, 12, result.getDayOfMonth());
-                        year.setValue(result.getYear());
+                    if (usedDate.getMonth().getValue() == 1) {
+                        usedDate = LocalDate.of(usedDate.getYear()-1, 12, usedDate.getDayOfMonth());
+                        year.setValue(usedDate.getYear());
                     }
                     else
-                        result = LocalDate.of(result.getYear(), result.getMonth().getValue()-1, result.getDayOfMonth());
+                        usedDate = LocalDate.of(usedDate.getYear(), usedDate.getMonth().getValue()-1, usedDate.getDayOfMonth());
                     update();
                     revalidate();
                 }
@@ -49,12 +62,12 @@ public class DatePicker extends JDialog {
             next.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (result.getMonth().getValue() == 12) {
-                        result = LocalDate.of(result.getYear()+1, 1, result.getDayOfMonth());
-                        year.setValue(result.getYear());
+                    if (usedDate.getMonth().getValue() == 12) {
+                        usedDate = LocalDate.of(usedDate.getYear()+1, 1, usedDate.getDayOfMonth());
+                        year.setValue(usedDate.getYear());
                     }
                     else
-                        result = LocalDate.of(result.getYear(), result.getMonth().getValue()+1, result.getDayOfMonth());
+                        usedDate = LocalDate.of(usedDate.getYear(), usedDate.getMonth().getValue()+1, usedDate.getDayOfMonth());
                     update();
                     revalidate();
                 }
@@ -70,7 +83,7 @@ public class DatePicker extends JDialog {
             year.addChangeListener(new ChangeListener(){
                 @Override
                 public void stateChanged(ChangeEvent e) {
-                    result = LocalDate.of((int)year.getValue(), result.getMonth(), result.getDayOfMonth());
+                    usedDate = LocalDate.of((int)year.getValue(), usedDate.getMonth(), usedDate.getDayOfMonth());
                     update();
                     revalidate();
                 }
@@ -82,18 +95,39 @@ public class DatePicker extends JDialog {
 
         update();
     }
+    /**
+     * Constructor.
+     */
     public DatePicker() {
-        this(Locale.getDefault());
+        this(LocalDate.now(), Locale.getDefault());
     }
+    /**
+     * Constructor.
+     * @param lang the language
+     */
+    public DatePicker(Locale lang) {
+        this(LocalDate.now(), lang);
+    }
+    /**
+     * Constructor.
+     * @param date the starting date
+     */
+    public DatePicker(LocalDate date) {
+        this(date, Locale.getDefault());
+    }
+    /**
+     * The resulting date.
+     * @return the date picked by the user. null if no date has been choosen.
+     */
     public LocalDate getResult() {
         return result;
     }
     private void update() {
         days.removeAll();
-        LocalDate firstDayOfMonth = LocalDate.of(result.getYear(), result.getMonth().getValue(), 1);
+        LocalDate firstDayOfMonth = LocalDate.of(usedDate.getYear(), usedDate.getMonth().getValue(), 1);
         int cases;
         {
-            int daysDisplayed = result.lengthOfMonth();
+            int daysDisplayed = usedDate.lengthOfMonth();
             daysDisplayed += firstDayOfMonth.getDayOfWeek().getValue()-1;
             cases = (1 + (int)Math.ceil(daysDisplayed/7f))*7;
             days.setLayout(new GridLayout(1 + (int)Math.ceil(daysDisplayed/7f), 7));
@@ -106,14 +140,14 @@ public class DatePicker extends JDialog {
             days.add(new JLabel());
             cases--;
         }
-        for (int i = 0;i<result.lengthOfMonth();i++) {
+        for (int i = 0;i<usedDate.lengthOfMonth();i++) {
             JButton currentDay = new JButton(Integer.valueOf(i+1).toString());
             final int I = i;
             final Window itself = this;
             currentDay.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    result = LocalDate.of(result.getYear(), result.getMonth(), I+1);
+                    result = LocalDate.of(usedDate.getYear(), usedDate.getMonth(), I+1);
                     itself.dispatchEvent(new WindowEvent(itself, WindowEvent.WINDOW_CLOSING));
                 }
 
@@ -125,6 +159,6 @@ public class DatePicker extends JDialog {
             days.add(new JLabel());
             cases--;
         }
-        month.setText(DateTimeFormatter.ofPattern("MMMM", language).format(result));
+        month.setText(DateTimeFormatter.ofPattern("MMMM", language).format(usedDate));
     }
 }
